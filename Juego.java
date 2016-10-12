867449021f92c27ad86c5bb591024104923898a7
jackrode31 <rode31x@gmail.com>
2016-10-12 10:43:28 -0500

    Trabajando en el Main

--- Juego.java
+++ Juego.java
@@ -1,8 +1,41 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Juego {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		TicTacToe game = new TicTacToe();
+		
+		
+		System.out.println("\t¡¡¡¡¡Bienvenido al juego, chaval!!!!\t");
+		System.out.println("Ingrese fila columna"
+				+ "recuerde ingresar filas y columnas en un intervalo de 1 a 3");
+		
+		System.out.println("Fila:");
+		int row=Integer.parseInt(br.readLine());
+		System.out.println("Columna:");
+		
+		int col=Integer.parseInt(br.readLine());
+		game.placeMark(row, col);
+		
+		game.ImprimirPartida();
+	
+		
+		if (game.checkForWin()) {
+		   System.out.println("We have a winner! Congrats!");
+		   game.ImprimirPartida();
+		   System.exit(0);
+		}
+		else if (game.TableroLleno()) {
+		  if (game.CheckEmpate());{
+			  System.out.println("Appears we have a draw!");
+			  game.ImprimirPartida();
+		  }
+		   System.exit(0);
+		}
+		
+
 
 	}
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,105 @@
 
 public class TicTacToe {
 
+	private char [][] tablero;
+	private char jugador;
+	
+	public TicTacToe(){
+		tablero = new char [3][3];
+		jugador='x';
+		iniciartablero();
+		
+		}
+	public void iniciartablero(){
+		 for (int i = 0; i < 3; i++){
+			 for (int j = 0; j < 3; j++){
+			   tablero[i][j] = '-';
+
+			 }
+		 }
+	}
+	public void ImprimirPartida(){
+		  System.out.println("-------------");
+		          for (int i = 0; i < 3; i++) {
+		              System.out.print("| ");
+		              for (int j = 0; j < 3; j++) {
+		                  System.out.print(tablero[i][j] + " | ");
+		              }
+		              System.out.println();
+		              System.out.println("-------------");
+		          }
+		      }
+
+	public boolean TableroLleno(){
+		boolean TableroLleno= true;
+		 for (int i = 0; i < 3; i++){
+			 for (int j = 0; j < 3; j++){
+			   if(tablero[i][j] == '-');{
+				   TableroLleno=false;
+			   }
+
+			 }
+		 }
+		return TableroLleno;
+	}
+	public char NuevoTurno(char jugador) {
+	    this.jugador=jugador;
+		char nuevoTurno='e';
+	    if (jugador == 'o')
+	        nuevoTurno='x';
+	    if (jugador == 'x')
+	        nuevoTurno='o';
+	    return nuevoTurno;
+	}
+	public boolean CheckEmpate() {
+	    for (int i = 0; i < 3; i++)
+	        for (int p=0; p < 3; p++)
+	            if(tablero [i][p]==' ')
+	                return false;
+
+	    return true;
+	}
+	public boolean checkForWin() {
+		        return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
+		    }
+	private boolean checkRowsForWin() {
+		        for (int i = 0; i < 3; i++) {
+		            if (checkRowCol(tablero[i][0], tablero[i][1], tablero[i][2]) == true) {
+		                return true;
+		            }
+		        }
+		        return false;
+		    }
+		     
+
+
+	 private boolean checkColumnsForWin() {
+		         for (int i = 0; i < 3; i++) {
+		             if (checkRowCol(tablero[0][i], tablero[1][i], tablero[2][i]) == true) {
+		                 return true;
+		             }
+		         }
+		         return false;
+		     }
+	 
+		     private boolean checkDiagonalsForWin() {
+		         return ((checkRowCol(tablero[0][0], tablero[1][1], tablero[2][2]) == true) || (checkRowCol(tablero[0][2], tablero[1][1], tablero[2][0]) == true));
+		     }
+		     
+		     private boolean checkRowCol(char c1, char c2, char c3) {
+		         return ((c1 != '-') && (c1 == c2) && (c2 == c3));
+		     }
+		     public boolean placeMark(int row, int col) {
+		    	         if ((row >= 0) && (row < 3)) {
+		    	             if ((col >= 0) && (col < 3)) {
+		    	                 if (tablero[row][col] == '-') {
+		    	                     tablero[row][col] = jugador;
+		    	                     return true;
+		    	                 }
+		    	             }
+		    	         }
+		    	         return false;
+		    	     }
+
 }
+
